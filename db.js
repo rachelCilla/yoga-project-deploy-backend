94d7f2aa094886735f8f378b001f33ed0687482c
Rachel Cilla <104958634+rachelCilla@users.noreply.github.com>
2023-09-01 12:38:56 -0600

    ready to deploy

--- db.js
+++ db.js
@@ -1,28 +1,30 @@
 // module used in server.js
 
-
 // from postgres documentation
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
-require('dotenv').config() 
+const { Pool } = require("pg");
+
+require("dotenv").config();
 
 // how we are going to communicate with our postgres database
-// Pool/pg is from pg package 
-const pool = new Pool({
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DBPORT,
-    database: process.env.DATABASE
-})
 
-{// INFO module.exports: 
-//  a module is a file that contains code that can be loaded and used in other files in a Node.js application. T
-// the module obj is a special obj in node.js that repressents the current module
-// in this case, the module is db.js
-// exports property of the module obj is an obj that is used to export values from the module
-// pool obj is assigned to the exports property of the module obj 
-//  this makes it available to other modules that require the db.js file module using require()
+const pool = new Pool({
+	user: process.env.USERNAME,
+	password: process.env.PASSWORD,
+	host: process.env.HOST,
+	port: process.env.DBPORT,
+	database: process.env.DATABASE,
+});
+
+{
+	// INFO module.exports:
+	//  a module is a file that contains code that can be loaded and used in other files in a Node.js application. T
+	// the module obj is a special obj in node.js that repressents the current module
+	// in this case, the module is db.js
+	// exports property of the module obj is an obj that is used to export values from the module
+	// pool obj is assigned to the exports property of the module obj
+	//  this makes it available to other modules that require the db.js file module using require()
 }
 
 module.exports = pool;
